3381bcc32340db2cd072d053567412f58f304ca1
Leonardo Augusto Faria <leonardo.cobrinha@gmail.com>
2015-03-10 02:02:35 -0300

    More adjustments to build MoKee

--- device.mk
+++ device.mk
@@ -246,6 +246,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
 
-# CMActions
+# MKActions
 PRODUCT_PACKAGES += \
-    CMActions
+    MKActions
